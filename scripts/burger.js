7d1095d92a86eedb0b096262b52f002584ce4715
jaana <jaana.jegorova@gmail.com>
2025-06-29 21:40:39 +0300

    small fix for input fields hiding, when burger-menu is pressed

--- scripts/burger.js
+++ scripts/burger.js
@@ -4,6 +4,7 @@ const burger = document.querySelector("#burger");
 
 // const linksInBurger = document.querySelectorAll("#burger a");
 const linksWrapperInBurger = document.querySelector("#burger ul");
+const dateFields = document.querySelectorAll('#departure-date-from, #departure-date-to');
 
 let opened = false;
 
@@ -11,12 +12,14 @@ function openBurgerMenu() {
     burgerIcon.setAttribute("src", "img/burger-close.png");
     burger.classList.remove("burger__closed");
     burger.classList.add("burger__opened");
+    dateFields.forEach(input => input.style.display = 'none'); //hidden date fields
 }
 
 function closeBurgerMenu() {
     burgerIcon.setAttribute("src", "img/burger-open.png");
     burger.classList.add("burger__closed");
     burger.classList.remove("burger__opened");
+    dateFields.forEach(input => input.style.display = ''); //present date fields again
 }
 
 burgerOpenerButton.addEventListener("click", function (event) {
